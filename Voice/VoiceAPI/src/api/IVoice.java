13a6ecefe7fb9d63705adeec4432032c7e89536a
Tyler Manning <sylsr9@gmail.com>
2017-11-08 16:58:58 -0700

    Setup API #48

---
+++ Voice/VoiceAPI/src/api/IVoice.java
@@ -0,0 +1,15 @@
+package api;
+import java.io.File;
+
+public interface IVoice {
+	
+	/***
+	 * This is the front facing interface for the voice recognition packages.
+	 * A .wav file of the subjects voice recording needs to be passed in,
+	 * as well as what the subject was intending to say.
+	 * @param wavFile The voice data of the subject (file handle).
+	 * @param originalInput A string of text that represents the "game" the subject attempted to say.
+	 * @return VoiceMetaData
+	 */
+	public VoiceMetaData analyze(File wavFile, String originalInput);
+}
---
+++ Voice/VoiceAPI/src/api/Voice.java
@@ -0,0 +1,12 @@
+package api;
+import java.io.File;
+
+public class Voice implements IVoice{
+
+	@Override
+	public VoiceMetaData analyze(File wavFile, String originalInput) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
