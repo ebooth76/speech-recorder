d1e4a412a7e99a377919846a75c20e4dea832217
phenggeler <peterhenggeler@u.boisestate.edu>
2017-11-07 17:45:33 -0700

    Adding user.java to serve as the model

---
+++ src/main/java/app/User.java
@@ -0,0 +1,42 @@
+package app;
+
+public class User {
+
+    private long id;
+    private String username;
+    private String password;
+    private String user_type;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUserName(String username) {
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getUserType(){
+        return user_type;
+    }
+
+    public void setUserType(){
+        this.user_type = user_type;
+    }
+
+}
