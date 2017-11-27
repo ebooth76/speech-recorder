c48355848c6b5f69f174c0e7cc005f65b1e11179
Aaron Wamsley author@aaronwamsley@u.boisestate.edu <Aaron Wamsley author@aaronwamsley@u.boisestate.edu>
2017-11-27 15:01:17 -0700

    #53 began fleshing out details in classes as they're received.

--- src/main/java/app/ReceiveAudio.java
+++ src/main/java/app/ReceiveAudio.java
@@ -1,13 +1,19 @@
 package src.main.java.app;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
+
+import Voice.VoiceAPI.src.api.*;
 
 /**
  * @author Aaron Wamsley
  *
  */
-public class ReceiveAudio {
+public class ReceiveAudio{
 	
 	//globals
 	private byte[] buffer;
@@ -39,29 +45,56 @@ public class ReceiveAudio {
 	
 	/**
 	 * Save audio to disk and insert database reference to it.
+	 * returns: 	0=success
+	 * 				1=failed to open output stream
 	 */
-	private void saveAudio() {
+	private FileReturn saveAudio() {
 		//TODO create path to save to
-		
+		String path = "where to save";
 		//TODO generate file name
-		
+		String name = user + System.currentTimeMillis();
 		//TODO save file name to path
-		
+		String saveFile = path + "\\" + name;
+		try {
+			FileOutputStream fileOut = new FileOutputStream(saveFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return new FileReturn(1, null);
+		}
 		//TODO insert entry in DB
-		
+		return new FileReturn(0, saveFile);
 	}
 	
 	/**
-	 * Sphinx requires audio be in the following format:
+	 * Sphinx requires audio file be in the following format:
 	 * RIFF, Little Endian, WAVE audio, Microsoft PCM, 16 bit, mono 1600Hz
 	 */
-	private void sendToSphinx() {
+	private VoiceMetaData sendToSphinx(File f) {
+		Voice voice = new Voice();
+		
 		//TODO convert audio input to Sphinx required format (if necessary)
 		
 		//TODO transmit to Sphinx
+		return voice.analyze(f, prompt);		
+	}
+	
+	/**
+	 * helper class to handle return from file creation.
+	 *
+	 */
+	private class FileReturn {
+		private int returnCode;
+		private String fileName;
 		
-		//TODO process Sphinx return
-		//TODO change method return value based on Sphinx return type.
-		
+		public FileReturn(int i, String s) {
+			returnCode = i;
+			fileName = s;
+		}
+		public int getReturnCode() {
+			return returnCode;
+		}
+		public String getFileName() {
+			return fileName;
+		}
 	}
 }
