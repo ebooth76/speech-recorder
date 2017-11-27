4244c2c2727ed4b28d6054f952efae2e4bb49e95
Aaron Wamsley <author@aaronwamsley@u.boisestate.edu>
2017-11-27 13:34:32 -0700

    Stubbed out methods in more detail.  noted sphinx requirements.  still waiting on details as to how it will be recorded by front end team.

--- src/main/java/app/ReceiveAudio.java
+++ src/main/java/app/ReceiveAudio.java
@@ -12,26 +12,55 @@ public class ReceiveAudio {
 	//globals
 	private byte[] buffer;
 	private int port;
-	private static AudioInputStream input;
-	private String prompt;
+	private static AudioInputStream audioIn;
+	private String prompt, user;
 	
 	
 	/**
 	 * @param in - audio input stream
 	 * @param p - prompt the user is attempting to match.
 	 */
-	public ReceiveAudio(AudioInputStream in, String p) {
+	public ReceiveAudio(AudioInputStream in, String p, String u) {
 		/*******Audio device settings***********/
-		private float rate = 44100.0f;
-		private int sampleSize = 16;
-		private int channels = 2;
-		private boolean isSigned = true;
-		private boolean bigEndian = true;
+		float rate = 44100.0f;
+		int sampleSize = 16;
+		int channels = 2;
+		boolean isSigned = true;
+		boolean bigEndian = true;
 		/***************************************/
 		
-		input = in;
+		audioIn = in;
 		prompt = p;
+		user = u;
 		AudioFormat af = new AudioFormat(rate, sampleSize, channels, isSigned, bigEndian);
+		
+	}
+	
+	/**
+	 * Save audio to disk and insert database reference to it.
+	 */
+	private void saveAudio() {
+		//TODO create path to save to
+		
+		//TODO generate file name
+		
+		//TODO save file name to path
+		
+		//TODO insert entry in DB
+		
+	}
+	
+	/**
+	 * Sphinx requires audio be in the following format:
+	 * RIFF, Little Endian, WAVE audio, Microsoft PCM, 16 bit, mono 1600Hz
+	 */
+	private void sendToSphinx() {
+		//TODO convert audio input to Sphinx required format (if necessary)
+		
+		//TODO transmit to Sphinx
+		
+		//TODO process Sphinx return
+		//TODO change method return value based on Sphinx return type.
 		
 	}
 }
