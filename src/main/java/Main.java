956dafdc8332043b2f6352820a647a54c30863e2
phenggeler <peterhenggeler@u.boisestate.edu>
2017-12-03 12:30:44 -0700

    checking why db display is having issues #17

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -111,7 +111,7 @@ public class Main {
       Statement stmt = connection.createStatement();
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
       stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks;");
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
@@ -160,7 +160,7 @@ public class Main {
       }
 
       model.put("records", output);
-      return "db";
+      return "game";
     } catch (Exception e) {
       model.put("message", e.getMessage());
       return "error";
