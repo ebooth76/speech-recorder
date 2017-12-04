e68234459b4a89181c1f5c669216338c6a54b30c
Aaron Wamsley author@aaronwamsley@u.boisestate.edu <Aaron Wamsley author@aaronwamsley@u.boisestate.edu>
2017-12-04 11:43:07 -0700

    removed references defunct test classes.

--- src/main/java/voice/VoiceAPI/src/analysis/VoiceAnalysis.java
+++ src/main/java/voice/VoiceAPI/src/analysis/VoiceAnalysis.java
@@ -6,9 +6,9 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import api.VoiceMetaData;
-//import edu.cmu.sphinx.api.Configuration;
-//import edu.cmu.sphinx.api.SpeechResult;
-//import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 public class VoiceAnalysis{
 
 	public VoiceMetaData analyze(File wavFile, String refText, VoiceMetaData vData) throws IOException{
