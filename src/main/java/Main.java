90231b78d6889f2b83e870f0ba8fc3905c1962fe
Stacy Black <stacyblack@u.boisestate.edu>
2017-12-03 18:58:17 -0700

    Removed unnecessary write-to-file code #60

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -193,11 +193,12 @@ String record(Map<String, Object> model) {
   @MessageMapping ("/audio")
   public void stream(String base64Audio) throws Exception {
     System.out.println("incoming message ...");
-    Base64.Decoder decoder = Base64.getDecoder();
-    byte[] decodedByte = decoder.decode(base64Audio.split(",")[1]);
-    FileOutputStream fos = new FileOutputStream("clip.wav");
-    fos.write(decodedByte);
-    fos.close();
+    // write to file
+//    Base64.Decoder decoder = Base64.getDecoder();
+//    byte[] decodedByte = decoder.decode(base64Audio.split(",")[1]);
+//    FileOutputStream fos = new FileOutputStream("clip.wav");
+//    fos.write(decodedByte);
+//    fos.close();
   }
 
 /*
