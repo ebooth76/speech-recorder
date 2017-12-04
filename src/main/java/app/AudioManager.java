7b5d5020e83269c9615bb6ea800d32ecdff797f6
Aaron Wamsley author@aaronwamsley@u.boisestate.edu <Aaron Wamsley author@aaronwamsley@u.boisestate.edu>
2017-12-03 22:33:45 -0700

    trying different import statements.

--- src/main/java/app/AudioManager.java
+++ src/main/java/app/AudioManager.java
@@ -2,7 +2,8 @@ package app;
 
 import javax.sound.sampled.AudioInputStream;
 
-import Voice.VoiceAPI.src.api.VoiceMetaData;
+import src.Voice.VoiceAPI.src.api.VoiceMetaData;
+import src.Voice.VoiceAPI.src.api.Voice;
 
 import java.io.File;
 import java.io.FileNotFoundException;
