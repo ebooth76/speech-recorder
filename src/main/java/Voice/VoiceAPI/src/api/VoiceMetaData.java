0cb31a5666b03a3d214959ba5cad2f22499d90e5
Tyler Manning <sylsr9@gmail.com>
2017-12-06 23:13:44 -0700

    Moved voice stuff around & finished #63 & #65

--- src/main/java/Voice/VoiceAPI/src/api/VoiceMetaData.java
+++
@@ -1,5 +0,0 @@
-package Voice.VoiceAPI.src.api;
-
-public class VoiceMetaData {
-
-}
---
+++ src/voice/analysis/Test.java
@@ -0,0 +1,14 @@
+package voice.analysis;
+
+import Voice.VoiceAPI.src.api.VoiceMetaData;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Test {
+	public static void main(String[]args) throws IOException {
+		File file = new File("C:\\Users\\Tyler\\workspace\\SeniorProject\\CS471-F17-Micronophones\\Voice\\VoiceAPI\\data\\10001-90210-01803.wav");
+		VoiceAnalysis va = new VoiceAnalysis();
+		va.analyze(file, "foo", new VoiceMetaData());
+	}
+}
