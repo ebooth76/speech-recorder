d6452996a90c5288522e1c28c3f790122e97061c
Aaron Wamsley author@aaronwamsley@u.boisestate.edu <Aaron Wamsley author@aaronwamsley@u.boisestate.edu>
2017-12-08 16:41:01 -0700

    #61 - re-implemented authentication without jetty.

--- src/main/java/app/GoogleDrive.java
+++ src/main/java/app/GoogleDrive.java
@@ -5,6 +5,7 @@ import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInsta
 //import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -15,98 +16,103 @@ import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.*;
 import com.google.api.services.drive.Drive;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-//
+
 public class GoogleDrive {
-//
-//	
-//	/** Application Name **/
-//	final private static String APPLICATION_NAME = "Micronophones";
-//    /** Directory to store user credenti
-//     * als for this application. */
-//    private static final java.io.File DATA_STORE_DIR = new java.io.File(
-//        System.getProperty("user.home"), ".credentials/drive-java-quickstart");
-//
-//    /** Global instance of the {@link FileDataStoreFactory}. */
-//    private static FileDataStoreFactory DATA_STORE_FACTORY;
-//
-//    /** Global instance of the JSON factory. */
-//    private static final JsonFactory JSON_FACTORY =
-//        JacksonFactory.getDefaultInstance();
-//
-//    /** Global instance of the HTTP transport. */
-//    private static HttpTransport HTTP_TRANSPORT;
-//
-//    private static Drive service; 
-//    /** Global instance of the scopes required by this quickstart.
-//     *
-//     * If modifying these scopes, delete your previously saved credentials
-//     * at ~/.credentials/drive-java-quickstart
-//     */
-//    private static final List<String> SCOPES =
-//        Arrays.asList(DriveScopes.DRIVE_METADATA_READONLY);
-//
-//    static {
-//        try {
-//            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-//            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-//        } catch (Throwable t) {
-//            t.printStackTrace();
-//            System.exit(1);
-//        }
-//    }
-//
-//    /**
-//     * Creates an authorized Credential object.
-//     * @return an authorized Credential object.
-//     * @throws IOException
-//     */
-//    public static Credential authorize() throws IOException {
-//        // Load client secrets.
-//        InputStream in =
-//            GoogleDrive.class.getResourceAsStream("/Micronophones-3182605bb4bb.json");
-//        GoogleClientSecrets clientSecrets =
-//            GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-//
-//        // Build flow and trigger user authorization request.
-//        GoogleAuthorizationCodeFlow flow =
-//                new GoogleAuthorizationCodeFlow.Builder(
-//                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-//                .setDataStoreFactory(DATA_STORE_FACTORY)
-//                .setAccessType("offline")
-//                .build();
-//        Credential credential = new AuthorizationCodeInstalledApp(
-//            flow, new LocalServerReceiver()).authorize("user");
-//        return credential;
-//    }
-//
-//    /**
-//     * Build and return an authorized Drive client service.
-//     * @return an authorized Drive client service
-//     * @throws IOException
-//     */
-//    public static Drive getDriveService() throws IOException {
-//        Credential credential = authorize();
-//        return new Drive.Builder(
-//                HTTP_TRANSPORT, JSON_FACTORY, credential)
-//                .setApplicationName(APPLICATION_NAME)
-//                .build();
-//    }
+
+	  /** Global configuration of Google Cloud Storage OAuth 2.0 scope. */
+	  private static final String STORAGE_SCOPE =
+	      "https://www.googleapis.com/auth/drive.read_write";
+	/** Application Name **/
+	final private static String APPLICATION_NAME = "Micronophones";
+    /** Directory to store user credenti
+     * als for this application. */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+        System.getProperty("user.home"), ".credentials/drive-java-quickstart");
+
+    /** Global instance of the {@link FileDataStoreFactory}. */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY =
+        JacksonFactory.getDefaultInstance();
+
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport HTTP_TRANSPORT;
+
+    private static Drive service; 
+    /** Global instance of the scopes required by this quickstart.
+     *
+     * If modifying these scopes, delete your previously saved credentials
+     * at ~/.credentials/drive-java-quickstart
+     */
+    private static final List<String> SCOPES =
+        Arrays.asList(DriveScopes.DRIVE_METADATA_READONLY);
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Creates an authorized Credential object.
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
+    public static Credential authorize() throws IOException {
+        // Load client secrets.
+        InputStream in =
+            GoogleDrive.class.getResourceAsStream("/Micronophones-3182605bb4bb.json");
+        GoogleClientSecrets clientSecrets =
+            GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(DATA_STORE_FACTORY)
+                .setAccessType("offline")
+                .build();
+       //new Credential(BearerToken.authorizationHeaderAccessMethod()).setAccessToken(accessToken);
+        Credential credential = new AuthorizationCodeInstalledApp(
+           flow, null).authorize("user");
+        return credential;
+    }
+
+    /**
+     * Build and return an authorized Drive client service.
+     * @return an authorized Drive client service
+     * @throws IOException
+     */
+    public static Drive getDriveService() throws IOException {
+        Credential credential = authorize();
+        return new Drive.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
     
 	public GoogleDrive() {
-//		try {
-//			service = getDriveService();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		try {
+			service = getDriveService();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-//	public boolean saveFile(File f) {
-//		return false;
-//	}
+	public boolean saveFile(File f) {
+		return false;
+	}
 }
