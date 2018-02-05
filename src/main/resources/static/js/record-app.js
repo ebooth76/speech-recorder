e05ebece76e681dcee7a78c1da81420ed5e4660d
Stacy Black <stacyblack@u.boisestate.edu>
2018-02-05 12:12:36 -0700

    Added visualizer to recording page, closes #78

--- src/main/resources/static/js/record-app.js
+++ src/main/resources/static/js/record-app.js
@@ -2,16 +2,17 @@
 var record = document.querySelector('.record');
 var stop = document.querySelector('.stop');
 var soundClips = document.querySelector('.sound-clips');
+var canvas = document.querySelector('.visualizer');
 var mainSection = document.querySelector('.main-controls');
 
 // disable stop button while not recording
 stop.disabled = true;
 
 // visualiser setup - create web audio api context and canvas
-//var audioCtx = new (window.AudioContext || webkitAudioContext)();
-//var canvasCtx = canvas.getContext("2d");
+var audioCtx = new (window.AudioContext || webkitAudioContext)();
+var canvasCtx = canvas.getContext("2d");
 
-//main block for doing the audio recording
+// main block for doing the audio recording
 if (navigator.mediaDevices.getUserMedia) {
 
 	console.log('getUserMedia supported.');
@@ -22,7 +23,7 @@ if (navigator.mediaDevices.getUserMedia) {
 	var onSuccess = function(stream) {
 	var mediaRecorder = new MediaRecorder(stream);
 
-	//visualize(stream);
+	visualize(stream);
 
 	record.onclick = function() {
 		mediaRecorder.start();
@@ -134,58 +135,58 @@ navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
    console.log('getUserMedia not supported on your browser!');
 }
 
-//function visualize(stream) {
-//
-//	var source = audioCtx.createMediaStreamSource(stream);
-//
-//	var analyser = audioCtx.createAnalyser();
-//	analyser.fftSize = 2048;
-//	var bufferLength = analyser.frequencyBinCount;
-//	var dataArray = new Uint8Array(bufferLength);
-//
-//	source.connect(analyser);
-//	//analyser.connect(audioCtx.destination);
-//
-//	draw()
-//
-//	function draw() {
-//	WIDTH = canvas.width
-//	HEIGHT = canvas.height;
-//
-//	requestAnimationFrame(draw);
-//
-//	analyser.getByteTimeDomainData(dataArray);
-//
-//	canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-//	canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-//
-//	canvasCtx.lineWidth = 2;
-//	canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-//
-//	canvasCtx.beginPath();
-//
-//	var sliceWidth = WIDTH * 1.0 / bufferLength;
-//	var x = 0;
-//
-//	for(var i = 0; i &lt; bufferLength; i++) {
-//
-//		var v = dataArray[i] / 128.0;
-//		var y = v * HEIGHT/2;
-//
-//		if(i === 0) {
-//			canvasCtx.moveTo(x, y);
-//		} else {
-//			canvasCtx.lineTo(x, y);
-//		}
-//
-//		x += sliceWidth;
-//	}
-//
-//	canvasCtx.lineTo(canvas.width, canvas.height/2);
-//	canvasCtx.stroke();
-//
-//	}
-//}
+function visualize(stream) {
+
+	var source = audioCtx.createMediaStreamSource(stream);
+
+	var analyser = audioCtx.createAnalyser();
+	analyser.fftSize = 2048;
+	var bufferLength = analyser.frequencyBinCount;
+	var dataArray = new Uint8Array(bufferLength);
+
+	source.connect(analyser);
+	//analyser.connect(audioCtx.destination);
+
+	draw()
+
+	function draw() {
+	WIDTH = canvas.width
+	HEIGHT = canvas.height;
+
+	requestAnimationFrame(draw);
+
+	analyser.getByteTimeDomainData(dataArray);
+
+	canvasCtx.fillStyle = 'rgb(38, 64, 115)';
+	canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+	canvasCtx.lineWidth = 2;
+	canvasCtx.strokeStyle = 'rgb(230, 0, 57)';
+
+	canvasCtx.beginPath();
+
+	var sliceWidth = WIDTH * 1.0 / bufferLength;
+	var x = 0;
+
+	for(var i = 0; i < bufferLength; i++) {
+
+		var v = dataArray[i] / 128.0;
+		var y = v * HEIGHT/2;
+
+		if(i === 0) {
+			canvasCtx.moveTo(x, y);
+		} else {
+			canvasCtx.lineTo(x, y);
+		}
+
+		x += sliceWidth;
+	}
+
+	canvasCtx.lineTo(canvas.width, canvas.height/2);
+	canvasCtx.stroke();
+
+	}
+}
 
 //window.onresize = function() {
 //  canvas.width = mainSection.offsetWidth;
