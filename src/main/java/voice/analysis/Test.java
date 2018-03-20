d1c7e68eecec23342431edd4d170ccb1b33c9b16
lucaswestmoreland <lucaswestmoreland@u.boisestate.edu>
2018-03-19 18:43:24 -0600

    Finally got the reftext to phoneme conversion with Marshall. #87 closed

--- src/main/java/voice/analysis/Test.java
+++ src/main/java/voice/analysis/Test.java
@@ -9,7 +9,7 @@ import voice.api.VoiceMetaData;
 
 public class Test {
 	public static void main(String[] args) throws IOException {
-		File file = new File("E:\\CS471-F17-Micronophones\\10001-90210-01803.wav");
+		File file = new File("C:\\Users\\Alex\\IdeaProjects\\CS471-F17-Micronophones\\10001-90210-01803.wav");
 		VoiceAnalysis va = new VoiceAnalysis();
 		System.out.println("Starting Test...");
 		VoiceMetaData vData = new VoiceMetaData();
